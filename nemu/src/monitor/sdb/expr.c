8420ac88270d2b2337a18b2270fae343ea84003f
tracer-ysyx <tracer@ysyx.org>
2025-03-02 15:25:26 +0800

    > compile NEMU ysyx_22040000 胡更泓 Linux ysyx 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:25:26 up 11 min,  1 user,  load average: 0.00, 0.30, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -88,7 +88,7 @@ void init_regex() {
   }
 
   // @hgh: debug
-  make_token("(10 + 3) * (2 - 1)");
+  make_token("((10 + 3)) * ((2 - 1))");
   int ans = eval(0, cnt - 1);
 
   Log("HGHGH: %d", ans);
