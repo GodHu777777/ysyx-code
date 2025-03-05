045e27b478cbe0467bfdab870d74c465b3264b49
tracer-ysyx <tracer@ysyx.org>
2025-03-05 15:22:03 +0800

    > compile NEMU ysyx_22040000 胡更泓 Linux ysyx 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:22:03 up  6:17,  1 user,  load average: 0.41, 0.62, 0.39

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -152,12 +152,15 @@ static bool make_token(char *e) {
           case TK_NOTYPE: break;
           // to check if '*' is a multiplication or deref
           case '*': if(is_operator(tokens[cnt - 1].type) || tokens[cnt - 1].type == '(') tokens[cnt].type = TK_REF;break; 
+          case TK_HEX_NUM:
+            int value_in_decimal = strtol(substr_start, NULL, 16);
+            sprintf(substr_start, "%d", value_in_decimal);
           case TK_NUM: 
             for(int j = 0; j <= substr_len; j++) {
               tokens[cnt].str[j] = substr_start[j]; // save string
               if(j == substr_len) tokens[cnt].str[j] ='\0'; // clear
             }
-            
+          
           default: tokens[cnt].type = rules[i].token_type;
 
           // printf("HGHGH: 0x%x, %c\n", cnt, tokens[cnt].type);
